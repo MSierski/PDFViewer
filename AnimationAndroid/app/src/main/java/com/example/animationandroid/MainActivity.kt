c2ad9a024fdd73e4ae405bf9a85cc773d88ad333
MSierski <92363171+MSierski@users.noreply.github.com>
2022-11-24 11:04:33 +0100

    Add files via upload

---
+++ AnimationAndroid/app/src/main/java/com/example/animationandroid/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.animationandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fade_in.setOnClickListener {
+            textView.visibility = View.VISIBLE
+            val animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+            textView.startAnimation(animationFadeIn)
+        }
+        fade_out.setOnClickListener {
+            val animationFadeOut = AnimationUtils.loadAnimation(this, androidx.appcompat.R.anim.fade_out)
+            textView.startAnimation(animationFadeOut)
+            Handler().postDelayed({
+                textView.visibility = View.GONE
+            }, 1000)
+            }
+        zoom_in.setOnClickListener {
+            val animationZoomIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
+            textView.startAnimation(animationZoomIn)
+        }
+        zoom_out.setOnClickListener {
+            val animationZoomOut =
+                AnimationUtils.loadAnimation(this, R.anim.zoom_out)
+            textView.startAnimation(animationZoomOut)
+        }
+        slide_down.setOnClickListener {
+            val animationSlideDown = AnimationUtils.loadAnimation(this, R.anim.slide_in)
+            textView.startAnimation(animationSlideDown)
+        }
+        slide
+        _up.setOnClickListener {
+            val animationSlideUp = AnimationUtils.loadAnimation(this, R.anim.slide_out)
+            textView.startAnimation(animationSlideUp)
+        }
+        bounce.setOnClickListener {
+            val animationBounce = AnimationUtils.loadAnimation(this, R.anim.bounce)
+            textView.st
+            artAnimation(animationBounce)
+        }
+        rotate.setOnClickListener {
+            val animationRotate = AnimationUtils.loadAnimation(this, R.anim.rotate)
+            textView.startAnimation(animationRotate)
+        }
+    }
+}
